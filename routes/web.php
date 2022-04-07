da1ec833cf1cb48a802bdb0138019d3e10acdad9
aziyan99 <rajaazian08@gmail.com>
2022-04-07 22:14:12 +0700

    updating route

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(['prefix' => 'admin'], function () {
     Route::resource('/roles', RoleController::class)->except('show');
-    // Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
-    // Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
-    // Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
-    // Route::get('/roles/{role}', [RoleController::class, 'edit'])->name('roles.edit');
-    // Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
-    // Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 });
